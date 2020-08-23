b7801fc916604c30e62ed89c15cd53c4f3e65776
TTTriplicate <tttriplicate@gmail.com>
2020-08-23 06:46:53 -0400

    Abstracting area calculation

--- area.py
+++ area.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-def rect(perimeter):
-    side = perimeter/4
-    area = side ** 2
-    return area
+from math import tan, pi
 
-def tri(perimeter):
-    area = 3**(1/2)
-    side = perimeter/3
-    area = area * ((side**2)/4)
+def polygon(perimeter, sides):
+    area = sides * ((perimeter / sides) ** 2)
+    area /= (4 * tan(pi / sides))
     return area
