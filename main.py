d2ca4583206ee1c172896ff8021fc64c39896318
TTTriplicate <tttriplicate@gmail.com>
2020-08-23 07:14:33 -0400

    starting in on main

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import sys
+include area, perimeter
+
+k = (.2, .000001)
+equalAt = -1
+if len(sys.argv) < 2:#default case, find where 4 posts beats 3
+    while equalAt = -1:
+
