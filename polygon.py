0406a1d9025a21476d383279aad68d695a88af6f
TTTriplicate <tttriplicate@gmail.com>
2020-08-24 05:29:38 -0400

    adding descriptice comments

--- polygon.py
+++ polygon.py
@@ -3,11 +3,21 @@
 from math import tan, pi
 
 def area(perimeter, sides):
+    ```
+Calculates the maximum area of a polygon with a given perimeter and
+number of sides.  Since the maximum area will be had in the case of a 
+equilateral polygon, this formula can be used as a universal pattern
+    ```
     area = sides * ((perimeter / sides) ** 2)
     area /= (4 * tan(pi / sides))
     return area
 
 def perimeter(k, sides):
+    ```
+Based on the weighted value k and the number of sides, calculates
+the side length of an equilateral polygon with the given number
+of sides
+    ```
     if (k * sides) >= 1:
         return 0
     else:
